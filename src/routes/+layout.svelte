6e73130c9da98cacd0aa6084555fc99d9be4b99c
Eskil Wadsholt <eskw@stibosystems.com>
2022-10-19 07:40:42 +0200

    add minimal scroll example

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<main>
+    <slot>
+
+    </slot>
+    <nav>
+        <ul>
+            <li>
+                <a href="/">Home</a>
+                <a href="/child-scroll">Child Scroll</a>
+            </li>
+        </ul>
+    </nav>
+</main>
+
+<style>
+    nav {
+        position: fixed;
+        bottom: 0;
+    }
+</style>
---
+++ src/routes/child-scroll/+page.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+    const content = `This
+    is
+    just
+    a
+    simple
+    test
+    to
+    see
+    if
+    scroll
+    position
+    is
+    remembered
+    from
+    each
+    time
+    this
+    page
+    is
+    entered`
+
+    const debug = (s: string) => {
+        console.debug(s)
+        return s
+    } 
+</script>
+
+<h1>Child Scroll Page</h1>
+
+<h2>Scroll element</h2>
+<main>
+    {#each content.split('\n') as line}
+        <div class="flex-child">
+            {debug(line)}
+        </div>
+    {/each}
+</main>
+
+<style>
+    main {
+        width: 400px;
+        height: 400px;
+        padding: 5px 10px;
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+        overflow-y: auto;
+        background: #0005;
+    }
+    .flex-child {
+        padding: 20px;
+        border-radius: 40px;
+        background: lightblue;
+    }
+</style>
